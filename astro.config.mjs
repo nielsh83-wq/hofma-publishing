bdb87fc0e13227255e20ca1921ec87cc036e29ae
Niels Hofma <nielsh83@gmail.com>
2026-02-06 18:08:25 +0100

    Add site URL to config for sitemap generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  // This is required for the sitemap to generate absolute URLs
+  site: 'https://hofmapublishing.com',
+
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: [sitemap()]
 });
